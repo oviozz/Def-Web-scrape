e6edf451fc7fb31dc3921b3b0b70c6c619c6aa30
oviozz <42685801+oviozz@users.noreply.github.com>
2022-01-16 17:32:13 -0800

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -8,24 +8,23 @@ word = str(input("Definition: "))
 url = requests.get('https://www.vocabulary.com/dictionary/{}'.format(word)).text
 soup = BeautifulSoup(url,'lxml')
 before = soup.find('div',class_='word-area')
-after = before.find('p',class_='short').text
-long = before.find('p',class_='long').text
+try:
+    after = before.find('p', class_='short').text
+    long = before.find('p', class_='long').text
 
+    # gets synonyms
+    syn = soup.find('div', class_='defContent')
+    final = syn.find('a', class_='word').text
 
+    # prints out
+    print(after)
+    print('Synonyms: {}'.format(final))
+    print('---------------------------------------------------')
+    response = str(input("Do you want longer definition: ")).lower()
+    if response == 'yes':
+        print(long)
+    else:
+        print('No worries')
 
-#gets synonyms
-syn = soup.find('div',class_='defContent')
-final = syn.find('a',class_='word').text
-
-
-
-
-#prints out
-print(after)
-print('Synonyms: {}'.format(final))
-print('---------------------------------------------------')
-response = str(input("Do you want longer definition: ")).lower()
-if response == 'yes':
-    print(long)
-else:
-    print('No worries')
+except AttributeError:
+    print('Not a valid word')
